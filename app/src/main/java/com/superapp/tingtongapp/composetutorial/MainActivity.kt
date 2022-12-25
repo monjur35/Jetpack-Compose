26d8faf1dcabd8a1aa12b48ae4474e5475958ee9
monjur35 <monjur35-1019@diu.edu.bd>
2022-12-25 22:39:46 +0600

    first commit

---
+++ app/src/main/java/com/superapp/tingtongapp/composetutorial/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.superapp.tingtongapp.composetutorial
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.superapp.tingtongapp.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Conversation(messages = SampleData.conversationSample)
+        }
+    }
+}
+
+data class Message(val name: String, val age: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    Surface(
+        shape = MaterialTheme.shapes.medium,
+        elevation = 5.dp,
+        color = MaterialTheme.colors.background,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+
+
+    ) {
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(1.dp, MaterialTheme.colors.primaryVariant, CircleShape)
+
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = "${msg.name}!",
+                    fontFamily = FontFamily.Monospace,
+                    color = MaterialTheme.colors.secondaryVariant,
+                    style = MaterialTheme.typography.subtitle1
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+
+                Surface(
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Age: ${msg.age}!",
+                        fontFamily = FontFamily.Monospace,
+                        modifier = Modifier.padding(all = 4.dp),
+                        style = MaterialTheme.typography.body1,
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                    )
+
+                }
+
+            }
+        }
+    }
+
+
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Top App Bar")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Filled.List, "backIcon")
+                    }
+                },
+
+
+                )
+        }, content = {
+            LazyColumn {
+                items(messages) { message ->
+                    MessageCard(message)
+                }
+            }
+        })
+
+
+
+
+
+}
+
+@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTutorialTheme {
+        val a = Message("Monjur", "27")
+        //MessageCard(a)
+
+        Conversation(messages = SampleData.conversationSample)
+
+    }
+}
