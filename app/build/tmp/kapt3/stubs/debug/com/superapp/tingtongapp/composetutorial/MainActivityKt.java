41b76124ed7bb56703e87d2d077b65a0c2e97d2d
monjur35 <monjur35-1019@diu.edu.bd>
2022-12-27 01:34:28 +0600

    dagger hilt setup

---
+++ app/build/tmp/kapt3/stubs/debug/com/superapp/tingtongapp/composetutorial/MainActivityKt.java
@@ -0,0 +1,31 @@
+package com.superapp.tingtongapp.composetutorial;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import androidx.activity.ComponentActivity;
+import androidx.compose.foundation.layout.*;
+import androidx.compose.material.*;
+import androidx.compose.material.icons.Icons;
+import androidx.compose.runtime.*;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.tooling.preview.Preview;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 2, d1 = {"\u0000\u0014\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0002\b\u0004\u001a\u0016\u0010\u0000\u001a\u00020\u00012\f\u0010\u0002\u001a\b\u0012\u0004\u0012\u00020\u00040\u0003H\u0007\u001a\b\u0010\u0005\u001a\u00020\u0001H\u0007\u001a\u0010\u0010\u0006\u001a\u00020\u00012\u0006\u0010\u0007\u001a\u00020\u0004H\u0007\u00a8\u0006\b"}, d2 = {"Conversation", "", "messages", "", "Lcom/superapp/tingtongapp/composetutorial/Message;", "DefaultPreview", "MessageCard", "msg", "app_debug"})
+public final class MainActivityKt {
+    
+    @androidx.compose.runtime.Composable()
+    public static final void MessageCard(@org.jetbrains.annotations.NotNull()
+    com.superapp.tingtongapp.composetutorial.Message msg) {
+    }
+    
+    @androidx.compose.runtime.Composable()
+    public static final void Conversation(@org.jetbrains.annotations.NotNull()
+    java.util.List<com.superapp.tingtongapp.composetutorial.Message> messages) {
+    }
+    
+    @androidx.compose.runtime.Composable()
+    @androidx.compose.ui.tooling.preview.Preview(uiMode = android.content.res.Configuration.UI_MODE_NIGHT_YES, showBackground = true)
+    @androidx.compose.ui.tooling.preview.Preview(showBackground = true)
+    public static final void DefaultPreview() {
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/superapp/tingtongapp/composetutorial/response/gamesList/GamesListResponseItem.java
@@ -0,0 +1,182 @@
+package com.superapp.tingtongapp.composetutorial.response.gamesList;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0004\n\u0002\u0010\b\n\u0002\b!\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001B]\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0003\u0012\u0006\u0010\u0007\u001a\u00020\b\u0012\u0006\u0010\t\u001a\u00020\u0003\u0012\u0006\u0010\n\u001a\u00020\u0003\u0012\u0006\u0010\u000b\u001a\u00020\u0003\u0012\u0006\u0010\f\u001a\u00020\u0003\u0012\u0006\u0010\r\u001a\u00020\u0003\u0012\u0006\u0010\u000e\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u000fJ\t\u0010\u001d\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0003H\u00c6\u0003J\t\u0010 \u001a\u00020\u0003H\u00c6\u0003J\t\u0010!\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\"\u001a\u00020\u0003H\u00c6\u0003J\t\u0010#\u001a\u00020\bH\u00c6\u0003J\t\u0010$\u001a\u00020\u0003H\u00c6\u0003J\t\u0010%\u001a\u00020\u0003H\u00c6\u0003J\t\u0010&\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\'\u001a\u00020\u0003H\u00c6\u0003Jw\u0010(\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u00032\b\b\u0002\u0010\u0007\u001a\u00020\b2\b\b\u0002\u0010\t\u001a\u00020\u00032\b\b\u0002\u0010\n\u001a\u00020\u00032\b\b\u0002\u0010\u000b\u001a\u00020\u00032\b\b\u0002\u0010\f\u001a\u00020\u00032\b\b\u0002\u0010\r\u001a\u00020\u00032\b\b\u0002\u0010\u000e\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010)\u001a\u00020*2\b\u0010+\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010,\u001a\u00020\bH\u00d6\u0001J\t\u0010-\u001a\u00020\u0003H\u00d6\u0001R\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u0011R\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0012\u0010\u0011R\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0013\u0010\u0011R\u0011\u0010\u0006\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0014\u0010\u0011R\u0011\u0010\u0007\u001a\u00020\b\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0015\u0010\u0016R\u0011\u0010\t\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0017\u0010\u0011R\u0011\u0010\n\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0018\u0010\u0011R\u0011\u0010\u000b\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0019\u0010\u0011R\u0011\u0010\f\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001a\u0010\u0011R\u0011\u0010\r\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001b\u0010\u0011R\u0011\u0010\u000e\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u001c\u0010\u0011\u00a8\u0006."}, d2 = {"Lcom/superapp/tingtongapp/composetutorial/response/gamesList/GamesListResponseItem;", "", "developer", "", "freetogame_profile_url", "game_url", "genre", "id", "", "platform", "publisher", "release_date", "short_description", "thumbnail", "title", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getDeveloper", "()Ljava/lang/String;", "getFreetogame_profile_url", "getGame_url", "getGenre", "getId", "()I", "getPlatform", "getPublisher", "getRelease_date", "getShort_description", "getThumbnail", "getTitle", "component1", "component10", "component11", "component2", "component3", "component4", "component5", "component6", "component7", "component8", "component9", "copy", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class GamesListResponseItem {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String developer = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String freetogame_profile_url = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String game_url = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String genre = null;
+    private final int id = 0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String platform = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String publisher = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String release_date = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String short_description = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String thumbnail = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String title = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.superapp.tingtongapp.composetutorial.response.gamesList.GamesListResponseItem copy(@org.jetbrains.annotations.NotNull()
+    java.lang.String developer, @org.jetbrains.annotations.NotNull()
+    java.lang.String freetogame_profile_url, @org.jetbrains.annotations.NotNull()
+    java.lang.String game_url, @org.jetbrains.annotations.NotNull()
+    java.lang.String genre, int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String platform, @org.jetbrains.annotations.NotNull()
+    java.lang.String publisher, @org.jetbrains.annotations.NotNull()
+    java.lang.String release_date, @org.jetbrains.annotations.NotNull()
+    java.lang.String short_description, @org.jetbrains.annotations.NotNull()
+    java.lang.String thumbnail, @org.jetbrains.annotations.NotNull()
+    java.lang.String title) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public GamesListResponseItem(@org.jetbrains.annotations.NotNull()
+    java.lang.String developer, @org.jetbrains.annotations.NotNull()
+    java.lang.String freetogame_profile_url, @org.jetbrains.annotations.NotNull()
+    java.lang.String game_url, @org.jetbrains.annotations.NotNull()
+    java.lang.String genre, int id, @org.jetbrains.annotations.NotNull()
+    java.lang.String platform, @org.jetbrains.annotations.NotNull()
+    java.lang.String publisher, @org.jetbrains.annotations.NotNull()
+    java.lang.String release_date, @org.jetbrains.annotations.NotNull()
+    java.lang.String short_description, @org.jetbrains.annotations.NotNull()
+    java.lang.String thumbnail, @org.jetbrains.annotations.NotNull()
+    java.lang.String title) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getDeveloper() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getFreetogame_profile_url() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getGame_url() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getGenre() {
+        return null;
+    }
+    
+    public final int component5() {
+        return 0;
+    }
+    
+    public final int getId() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component6() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPlatform() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component7() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getPublisher() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component8() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getRelease_date() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component9() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getShort_description() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component10() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getThumbnail() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component11() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+}
