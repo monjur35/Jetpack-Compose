c60518e8eb5e4e7a858184381ae4d074438af26e
monjur35 <monjur35-1019@diu.edu.bd>
2022-12-29 23:31:38 +0600

    MVVM api call (not done)

--- app/build/tmp/kapt3/stubs/debug/com/superapp/tingtongapp/composetutorial/network/RetrofitBuilder.java
+++ app/build/tmp/kapt3/stubs/debug/com/superapp/tingtongapp/composetutorial/network/RetrofitBuilder.java
@@ -1,5 +1,6 @@
 package com.superapp.tingtongapp.composetutorial.network;
 
+import dagger.Provides;
 import dagger.hilt.InstallIn;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
--- app/src/main/java/com/superapp/tingtongapp/composetutorial/network/RetrofitBuilder.kt
+++ app/src/main/java/com/superapp/tingtongapp/composetutorial/network/RetrofitBuilder.kt
@@ -1,21 +1,32 @@
 package com.superapp.tingtongapp.composetutorial.network
 
+import com.superapp.tingtongapp.composetutorial.Repository
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
-@InstallIn
-@Singleton
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitBuilder {
     val baseUrl = "https://www.freetogame.com/api/"
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder().baseUrl(baseUrl)
-            .addConverterFactory(GsonConverterFactory.create()).build()
-    }
+    @Singleton
+    @Provides
+    private fun retrofit() = Retrofit.Builder().baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(ApiInterface::class.java)
 
-    private val apiInterface: ApiInterface by lazy {
-        retrofit.create(ApiInterface::class.java)
+
+    @Singleton
+    @Provides
+    fun provideRepository(api:ApiInterface) : Repository = DefualtMainRepository(api)
+
+    private fun DefualtMainRepository(api: ApiInterface): Repository {
+
     }
 }
